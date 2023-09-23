de980edfbea4514ba6ed32a3e2de0156dc94aedd
Huong <huogthu552@gmail.com>
2023-09-23 17:19:38 +0700

    them trang chu

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $provinces = \App\Models\Province::all();
+    return view('welcome', compact('provinces'));
 });
